045295c83530c655dd8805b158fda83cb8e9710f
mletunov <maxim.letunov@artezio.com>
2016-02-01 02:02:01 +0300

    Первые зарисовки

--- WinClient/Form1.cs
+++ WinClient/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Logic;
 
 namespace WinClient
 {
@@ -15,6 +16,11 @@ namespace WinClient
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            new Class1().Test();
         }
     }
 }
