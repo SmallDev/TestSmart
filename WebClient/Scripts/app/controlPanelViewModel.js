70046bfb2894c9b937eec2ba4df76c47e5ab72a5
Oleg Shchepotkin <subtext.dev@gmail.com>
2016-02-29 18:23:09 +0300

    небольшие доработки

--- WebClient/Scripts/app/controlPanelViewModel.js
+++ WebClient/Scripts/app/controlPanelViewModel.js
@@ -19,13 +19,28 @@
         }
     };
 
+    String.prototype.stringToSeconds = function () {
+        var secFromHours = Number(this.substring(0, 2)) * 1200;
+        var secFromMinuts = Number(this.substring(3, 5)) * 60;
+        var sec = Number(this.substring(6, 8));
+        var totalSec = secFromHours + secFromMinuts + sec;
+        return totalSec;
+    }
+
     function setControlData(that, data) {
         that.set("velocity", data.Velocity);
         that.set("allTime", data.AllTime);
 
+        var allTimeInSec = data.AllTime.stringToSeconds();
+        var calcTimeInSec = data.CalcTime.stringToSeconds();
+        var readTimeInSec = data.ReadTime.stringToSeconds();
+
+        var calcTimeInPercent = calcTimeInSec / allTimeInSec * 100;
+        var readTimeInPercent = readTimeInSec / allTimeInSec * 100;
+
         var gauge = $("#ctr-linear-progress").data("kendoLinearGauge");
-        gauge.pointers[0].value(Number(data.CalcTime.substring(3, 5)));
-        gauge.pointers[1].value(Number(data.ReadTime.substring(3, 5)));
+        gauge.pointers[0].value(calcTimeInPercent);
+        gauge.pointers[1].value(readTimeInPercent);
     }
 
     function getControlDataWorker(that) {
