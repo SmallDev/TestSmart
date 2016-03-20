5235b78dd9e37ac03a810c6492233529e86bab74
mletunov <maxim.letunov@artezio.com>
2016-03-20 19:40:59 +0300

    refactor clear and init sp

--- Database/Procedures/Clear.sql
+++ Database/Procedures/Clear.sql
@@ -1,20 +1,37 @@
 ﻿CREATE PROCEDURE [dbo].[Clear]
-	@users bit = 0,
-	@data bit = 0
+	@readData bit = 0,
+	@calcData bit = 0
 AS
 BEGIN
 	begin tran
-		if (@users = 1)
+		if (@readData = 1)
 		begin
-			delete from dbo.[UserProfile]
-			delete from dbo.[User]
-		end
+			truncate table dbo.[Data]
 
-		if (@data = 1)
-		begin
-			delete from dbo.[Data]
 			update dbo.[Settings] set Value = NULL
 			where Name = 'ReadTime'
+			update dbo.[Settings] set Value = NULL
+			where Name = 'ReadVelocity'
 		end
+
+		if (@calcData = 1)
+		begin tran
+			ALTER INDEX [PK_Profiles_NView] ON [dbo].[Profiles_NView] DISABLE
+
+			delete from dbo.[UserProfile]
+			delete from dbo.[User];DBCC CHECKIDENT ('dbo.[User]',RESEED, 0)
+			
+			delete from dbo.[ClusterNProfile]
+			delete from dbo.[ClusterRProfile]
+			delete from dbo.[Cluster];DBCC CHECKIDENT ('dbo.[Cluster]',RESEED, 0)
+
+			truncate table dbo.[Learning]
+			ALTER INDEX [PK_Profiles_NView] ON [dbo].[Profiles_NView] REBUILD
+
+			update dbo.[Settings] set Value = NULL
+			where Name = 'CalcTime'
+			update dbo.[Settings] set Value = NULL
+			where Name = 'CalcVelocity'
+		commit tran
 	commit
 END
--- Database/Procedures/InitClusters.sql
+++ Database/Procedures/InitClusters.sql
@@ -2,23 +2,31 @@
 	@count int
 AS
 BEGIN
+	declare @clusters table (Id int)
+
 	begin tran
-		insert into ClusterNProfile(ClusterId, PropertyId, NominalId, Probability)
-		select c.Id, p.Id, n.Id, RAND(CHECKSUM(NEWID())) from Cluster c, Properties p
-			join Nominal n on p.Id = n.PropertyId
-		where p.IsNominal = 1
+		insert into [Cluster](Name)
+		output inserted.Id INTO @clusters
+		select distinct n = number 
+		from master..[spt_values] 
+		where number BETWEEN 1 AND @count
+
+		insert into ClusterRProfile(ClusterId, PropertyId, Mean, Variance)
+		select c.Id, p.Id, 0, 1000 from @clusters c, Properties p
+		where p.IsNominal = 0
 
 		ALTER INDEX [PK_Profiles_NView] ON [dbo].[Profiles_NView] DISABLE
+
+		insert into ClusterNProfile(ClusterId, PropertyId, NominalId, Probability)
+		select c.Id, p.Id, n.Id, RAND(CHECKSUM(NEWID())) from @clusters c, Properties p
+			join Nominal n on p.Id = n.PropertyId
+		where p.IsNominal = 1
 
 		update cp
 		set Probability = Probability / gr.[Sum]
 		from ClusterNProfile cp
 			inner join (select ClusterId, PropertyId, sum(Probability) [Sum] from ClusterNProfile
 					 group by ClusterId, PropertyId) gr on cp.ClusterId = gr.ClusterId and cp.PropertyId = gr.PropertyId
-
-		insert into ClusterRProfile(ClusterId, PropertyId, Mean, Variance)
-		select c.Id, p.Id, 0, 1 from Cluster c, Properties p
-		where p.IsNominal = 0
 
 		ALTER INDEX [PK_Profiles_NView] ON [dbo].[Profiles_NView] REBUILD
 	commit
